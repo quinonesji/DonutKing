ea0238300927b4f46ba97ccfd01678ce9eac19f8
Jose Quinones <joseiq91@josequinones.home>
2020-04-10 13:14:55 -0400

    modifying connection strings

--- App/Config.php
+++ App/Config.php
@@ -9,30 +9,32 @@ namespace App;
  */
 class Config
 {
+    // Production MySQL database configuration
+    // mysql://b9624e29a9c8c6:4fb6127e@us-cdbr-iron-east-01.cleardb.net/heroku_1369e01860fbd29?reconnect=true
 
     /**
      * Database host
      * @var string
      */
-    const DB_HOST = 'localhost';
+    const DB_HOST = 'localhost';//us-cdbr-iron-east-01.cleardb.net
 
     /**
      * Database name
      * @var string
      */
-    const DB_NAME = 'DonutKing';
+    const DB_NAME = 'DonutKing';//heroku_1369e01860fbd29
 
     /**
      * Database user
      * @var string
      */
-    const DB_USER = 'app_king';
+    const DB_USER = 'app_king';//b9624e29a9c8c6
 
     /**
      * Database password
      * @var string
      */
-    const DB_PASSWORD = 'J456Kikd#k8@!';
+    const DB_PASSWORD = 'J456Kikd#k8@!';//4fb6127e
 
     /**
      * Show or hide error messages on screen
